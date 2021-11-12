deaf7b2c8d73645ec67d5948f07abe3ffe89a27e
Chimoha31 <mokochii1108@gmail.com>
2021-11-11 21:18:02 -0800

    create drawer

--- src/components/organism/layout/Header.tsx
+++ src/components/organism/layout/Header.tsx
@@ -1,9 +1,12 @@
 import { VFC, memo } from "react";
-import { Box, Flex, Heading, Link } from "@chakra-ui/react";
-// import { HamburgerIcon } from "@chakra-ui/icons";
+import { Box, Button, Drawer, DrawerBody, DrawerContent, DrawerOverlay, Flex, Heading, IconButton, Link, useDisclosure } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 export const Header: VFC = memo(() => {
+  const {isOpen, onOpen, onClose} = useDisclosure();
+
   return (
+    <>
     <Flex
       as="nav"
       bg="teal.500"
@@ -12,7 +15,7 @@ export const Header: VFC = memo(() => {
       justify="space-between"
       padding={{ base: 3, md: 5 }}
     >
-      <Flex align="center" as="a" mr={8} _hover={{cursor: "pointer"}}>
+      <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
         <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
           User Management App
         </Heading>
@@ -32,7 +35,27 @@ export const Header: VFC = memo(() => {
         </Box>
       </Flex>
 
-      {/* <IconButton aria-label= "menu-button" icon={<HamburgerIcon />} size="sm" variant="unstyled"/> */}
+      <IconButton
+        aria-label="menu-button"
+        icon={<HamburgerIcon />}
+        size="sm"
+        variant="unstyled"
+        display={{ base: "block", md: "none" }}
+        onClick={onOpen}
+      />
     </Flex>
+
+    <Drawer placement= "left" size="xs" onClose={onClose} isOpen={isOpen}>
+      <DrawerOverlay>
+        <DrawerContent>
+          <DrawerBody p={0} bg="gray.100">
+            <Button w="100%">TOP</Button>
+            <Button w="100%">User List</Button>
+            <Button w="100%">Setting</Button>
+          </DrawerBody>
+        </DrawerContent>
+      </DrawerOverlay>
+    </Drawer>
+    </>
   );
 });
