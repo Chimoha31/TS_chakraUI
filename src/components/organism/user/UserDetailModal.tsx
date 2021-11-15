5c5ea39dd1f878cfe6c2ed8d378f79dcd38557cd
Chimoha31 <mokochii1108@gmail.com>
2021-11-14 23:30:14 -0800

    used props for Modal

--- src/components/organism/user/UserDetailModal.tsx
+++ src/components/organism/user/UserDetailModal.tsx
@@ -1,14 +1,59 @@
-import { memo, VFC } from 'react';
+import React, { memo, VFC } from "react";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+} from "@chakra-ui/react";
 
 type Props = {
-
-}
+  isOpen: boolean;
+  onClose: () => void;
+};
 
 export const UserDetailModal: VFC<Props> = memo((props) => {
-  const {} = props
+  const { isOpen, onClose } = props;
 
-  return(
+  return (
     <>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        autoFocus={false}
+        motionPreset="slideInBottom"
+      >
+        <ModalOverlay />
+        <ModalContent pb={6}>
+          <ModalHeader>User Detail</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody mx={4}>
+            <Stack spacing={4}>
+              <FormControl>
+                <FormLabel>Name</FormLabel>
+                <Input value="Chiho" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Full Name</FormLabel>
+                <Input value="Chiho Maekawa" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>MAIL</FormLabel>
+                <Input value="12345@gmail.com" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>TELL</FormLabel>
+                <Input value="123-456-789" isReadOnly />
+              </FormControl>
+            </Stack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
   );
 });
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,11 +1,18 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { memo, useCallback, useEffect, VFC } from "react";
-import { Center, FormControl, FormLabel, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Input, Spinner, Stack, useDisclosure, Wrap, WrapItem } from "@chakra-ui/react";
+import React, { memo, useCallback, useEffect, VFC } from "react";
+import {
+  Center,
+  Spinner,
+  useDisclosure,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
 import { UserCard } from "../organism/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
+import { UserDetailModal } from "../organism/user/UserDetailModal";
 
 export const UserManagement: VFC = memo(() => {
-  const {isOpen, onOpen, onClose} = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const { getUsers, users, loading } = useAllUsers();
 
@@ -29,37 +36,11 @@ export const UserManagement: VFC = memo(() => {
                 fullName={user.name}
                 onClick={onClickUser}
               />
-          </WrapItem>
+            </WrapItem>
           ))}
         </Wrap>
       )}
-      <Modal isOpen={isOpen} onClose={onClose} autoFocus={false} motionPreset="slideInBottom" >
-        <ModalOverlay />
-        <ModalContent pb={6}>
-          <ModalHeader>User Detail</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody mx={4}>
-            <Stack spacing={4}>
-              <FormControl>
-                <FormLabel>Name</FormLabel>
-                <Input value="Chiho" isReadOnly/>
-              </FormControl>
-              <FormControl>
-                <FormLabel>Full Name</FormLabel>
-                <Input value="Chiho Maekawa" isReadOnly/>
-              </FormControl>
-              <FormControl>
-                <FormLabel>MAIL</FormLabel>
-                <Input value="12345@gmail.com" isReadOnly/>
-              </FormControl>
-              <FormControl>
-                <FormLabel>TELL</FormLabel>
-                <Input value="123-456-789" isReadOnly/>
-              </FormControl>
-            </Stack>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+      <UserDetailModal isOpen={isOpen} onClose={onClose} />
     </>
   );
 });
